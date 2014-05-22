beb527451031ecc49cbd2f1640031d07e6346b19
marios kosiaris <linkinpark_8884@hotmail.com>
2014-05-22 11:39:31 +0300

    first commit

---
+++ control/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(284, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(535, 448);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 648);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ control/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+using System.IO;
+
+
+namespace control
+{
+    public partial class Form1 : Form
+    {
+      
+        private int currentImageIndex = 0;
+
+        string[] arr1 = new string[] { "water", "eat", "bath", "sleep", "tv", "park", "START" };
+        string[] arr2 = new string[] { "nero", "fai", "mpanio", "sleep1", "tv1", "volta", "" };
+         
+    
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                check();
+              
+            //button1.BackgroundImage = Properties.Resources.water;
+
+                // Stop the timer if it runs already
+                if (timer1.Enabled)
+                {
+                    timer1.Stop();
+                    if (currentImageIndex != 0)
+                    {
+                        currentImageIndex -= 1;
+                    }
+                 
+                      Stream stream = Properties.Resources.ResourceManager.GetStream(arr2[currentImageIndex]);
+                        new SoundPlayer(stream).Play();
+                 
+                }
+
+                // Start the timer if it was stopped
+                else
+                {
+                    // Make the timer start right away
+                    currentImageIndex = 0;
+                    timer1.Interval = 1;
+
+                    // Start the timer
+                    timer1.Start();
+                }
+           
+        }
+     
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+         
+            timer1.Stop();
+            try
+            {
+                timer1.Interval = 5000; // Next time, wait 5 secs
+
+                // Set the image and select next picture
+                button1.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject(arr1[currentImageIndex]);
+
+                        Stream stream = Properties.Resources.ResourceManager.GetStream(arr2[currentImageIndex] );
+                         new SoundPlayer(stream).Play();
+            
+
+                currentImageIndex++;
+            }
+            finally
+            {
+                // Only start the timer if we have more images to show!
+                if (currentImageIndex < arr1.Length)
+                    timer1.Start();
+            }
+        }
+
+
+        private void check()
+        {
+            DateTime now = DateTime.Now;
+            DateTime morning = Convert.ToDateTime("06:00:00 AM");
+            DateTime morning1 = Convert.ToDateTime("12:00:00 PM");
+            DateTime noon = Convert.ToDateTime("12:00:00 PM");
+            DateTime evening = Convert.ToDateTime("16:00:00 PM");
+            DateTime night = Convert.ToDateTime("19:00:00 PM");
+ 
+
+            if ((now >= morning) && (now < morning1))
+            {
+                BackgroundImage = Properties.Resources.mpez;
+            }
+            else if ((now >= morning1) && (now < evening))
+            {
+                BackgroundImage = Properties.Resources.yellow;
+            }
+            else if ((now >= evening) && (now < night))
+            {
+                BackgroundImage = Properties.Resources.graylight;
+            }
+            else
+            {
+                BackgroundImage = Properties.Resources.bleu;
+             
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            check();
+         
+        }
+
+
+    }
+}
